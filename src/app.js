b62f2bcff46c9bca5d2d8357c9e5b2113621f670
Carlos <balaguer.88@gmail.com>
2020-12-25 19:09:10 +0100

    fix api run dev

--- src/app.js
+++ src/app.js
@@ -10,11 +10,15 @@ const app = express();
 
 // settings
 app.set("port", process.env.PORT || 3000);
+app.set("strict routing", true);
+app.enable('strict routing')
 
 // middlewares
 const corsOptions = {};
 app.use(cors(corsOptions))
+
 app.use(morgan('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -23,6 +27,6 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to my application" });
 });
 
-app.use(process.env.API, CodesRoutes);
+app.use('/api/codes', CodesRoutes);
 
 export default app;
